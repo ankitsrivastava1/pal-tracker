bbbf2d3bc00e5471b09c6a53fb027b0ec0e6d06c
ankit <asrivastava11@humana.com>
2019-02-05 16:02:39 -0500

    Adds info and health.

--- src/PalTracker/Startup.cs
+++ src/PalTracker/Startup.cs
@@ -11,6 +11,10 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Steeltoe.CloudFoundry.Connector.MySql.EFCore;
+using Steeltoe.Common.HealthChecks;
+using Steeltoe.Management.CloudFoundry;
+using Steeltoe.Management.Endpoint.CloudFoundry;
+using Steeltoe.Management.Endpoint.Info;
 
 namespace PalTracker
 {
@@ -41,7 +45,10 @@ namespace PalTracker
         services.AddDbContext<TimeEntryContext>(options => options.UseMySql(Configuration));
 
         services.AddScoped<ITimeEntryRepository, MySqlTimeEntryRepository>();
-		
+        services.AddCloudFoundryActuators(Configuration);
+		services.AddScoped<IHealthContributor, TimeEntryHealthContributor>();
+        services.AddSingleton<IOperationCounter<TimeEntry>, OperationCounter<TimeEntry>>();
+        services.AddSingleton<IInfoContributor, TimeEntryInfoContributor>();
 
         }
 
@@ -59,6 +66,7 @@ namespace PalTracker
 
             app.UseHttpsRedirection();
             app.UseMvc();
+            app.UseCloudFoundryActuators();
         }
     }
 }
